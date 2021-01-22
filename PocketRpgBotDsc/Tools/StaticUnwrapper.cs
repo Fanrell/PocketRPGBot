5271b36dc54c2e3047f58e748ffc7686760b78c5
fanrell <cieslik.kamil@outlook.com>
2021-01-22 11:54:50 +0100

    better secourity for random command pt 2

--- PocketRpgBotDsc/Tools/StaticUnwrapper.cs
+++ PocketRpgBotDsc/Tools/StaticUnwrapper.cs
@@ -17,10 +17,20 @@ namespace PocketRpgBotDsc.Tools
         public static int[] UwrapSeparator(string wraped, string separatro)
         {
             var unwraped = wraped.Split(separatro);
-            int[] toReturn = {
-                Convert.ToInt32(unwraped[0]),
-                Convert.ToInt32(unwraped[1])
-            };
+            int[] toReturn ;
+            try
+            {
+                toReturn = new int[] {
+                    Convert.ToInt32(unwraped[0]),
+                    Convert.ToInt32(unwraped[1])
+                };
+            }
+            catch
+            {
+                return new int[] {0,0};
+            }
+            if(toReturn[0] == null || toReturn[0] == 0)
+                toReturn[0] = 1;
             return toReturn;
         }
     }
